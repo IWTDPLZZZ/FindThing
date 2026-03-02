3e596304df4b97bcaf8a51b4ca356ab21d854fbe
Кавелич М.И. / гр.334701 <kavelichmatvey@gmail.com>
2026-03-02 16:02:35 +0300

    Добавление Firebase в конфигурацию программы

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:find_thing/firstPage.dart';
 import 'package:find_thing/authpage.dart';
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const FindThingApp());
 }
 
