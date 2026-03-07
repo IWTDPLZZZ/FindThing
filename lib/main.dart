f79aec4dc740a9388e24dc02b217ee4e3eee9fb4
Кавелич М.И. / гр.334701 <kavelichmatvey@gmail.com>
2026-03-07 09:51:47 +0300

    Добавление (готово) CustomAppBarForMainPage

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:find_thing/config/firebase_options.dart';
 import 'package:find_thing/screens/auth_page.dart';
 import 'package:find_thing/screens/first_page.dart';
-import 'package:find_thing/screens/final_page.dart';
+import 'package:find_thing/screens/third_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,13 +22,13 @@ class FindThingApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: Colors.white,
         scaffoldBackgroundColor: Colors.white,
-        appBarTheme: const AppBarTheme(),
-        textTheme: const TextTheme(),
-        inputDecorationTheme: const InputDecorationTheme(),
-        elevatedButtonTheme: const ElevatedButtonThemeData(),
-        textButtonTheme: const TextButtonThemeData(),
-        outlinedButtonTheme: const OutlinedButtonThemeData(),
-        iconTheme: const IconThemeData(),
+        // appBarTheme: const AppBarTheme(),
+        // textTheme: const TextTheme(),
+        // inputDecorationTheme: const InputDecorationTheme(),
+        // elevatedButtonTheme: const ElevatedButtonThemeData(),
+        // textButtonTheme: const TextButtonThemeData(),
+        // outlinedButtonTheme: const OutlinedButtonThemeData(),
+        // iconTheme: const IconThemeData(),
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         fontFamily: 'Inter',
       ),
